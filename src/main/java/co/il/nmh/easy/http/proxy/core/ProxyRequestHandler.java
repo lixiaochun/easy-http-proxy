8e0df9a7893ce9336a250024594715d126fb1c1a
Maor Hamami <mamaorha@gmail.com>
2018-08-22 11:28:35 +0300

    adding sort to url patterns

--- src/main/java/co/il/nmh/easy/http/proxy/core/ProxyRequestHandler.java
+++ src/main/java/co/il/nmh/easy/http/proxy/core/ProxyRequestHandler.java
@@ -1,9 +1,11 @@
 package co.il.nmh.easy.http.proxy.core;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,43 @@ public class ProxyRequestHandler
 {
 	@Autowired(required = false)
 	protected List<BaseProxyRequestHandler> proxyRequestHandlers;
+
+	@PostConstruct
+	private void init()
+	{
+		if (null != proxyRequestHandlers)
+		{
+			proxyRequestHandlers.sort(new Comparator<BaseProxyRequestHandler>()
+			{
+				@Override
+				public int compare(BaseProxyRequestHandler arg1, BaseProxyRequestHandler arg0)
+				{
+					Pattern pattern0 = arg0.urlPattern;
+					Pattern pattern1 = arg1.urlPattern;
+
+					boolean matches0 = pattern0.matcher(pattern1.pattern()).matches();
+					boolean matches1 = pattern1.matcher(pattern0.pattern()).matches();
+
+					if (!matches0 && !matches1)
+					{
+						return pattern1.pattern().compareTo(pattern0.pattern());
+					}
+
+					if (matches0 && matches1)
+					{
+						return 0;
+					}
+
+					if (matches0)
+					{
+						return -1;
+					}
+
+					return 1;
+				}
+			});
+		}
+	}
 
 	public ResponseEntity<Object> handle(HttpServletRequest httpServletRequest, String method, String requestURI, Map<String, List<String>> headers, byte[] payload)
 	{
