f2a3990b0256708126ab423d5d6b94aaff14f64e
Maor Hamami <mamaorha@gmail.com>
2018-12-31 11:39:29 +0200

    remove the @restController from proxyRes, it is defined in the configuration

--- src/main/java/co/il/nmh/easy/http/proxy/rest/ProxyRes.java
+++ src/main/java/co/il/nmh/easy/http/proxy/rest/ProxyRes.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import co.il.nmh.easy.http.proxy.core.ProxyRequestHandler;
 import co.il.nmh.easy.utils.EasyInputStream;
@@ -21,7 +20,6 @@ import co.il.nmh.easy.utils.EasyInputStream;
  * @author Maor Hamami
  *
  */
-@RestController
 public class ProxyRes
 {
 	@Autowired
